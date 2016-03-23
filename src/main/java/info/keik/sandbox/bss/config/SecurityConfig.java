6ddab29b9b73028e20736732ccc80650428fe95d
keik <k4t0.kei@gmail.com>
2016-03-24 00:58:02 +0900

    Add password encoder

--- src/main/java/info/keik/sandbox/bss/config/SecurityConfig.java
+++ src/main/java/info/keik/sandbox/bss/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package info.keik.sandbox.bss.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -18,7 +21,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth
-				.userDetailsService(userDetailsService);
+				.userDetailsService(userDetailsService)
+				.passwordEncoder(passwordEncoder());
 	}
 
 	@Override
@@ -35,6 +39,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 				.and()
 				.logout().logoutUrl("/logout").logoutSuccessUrl("/");
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
